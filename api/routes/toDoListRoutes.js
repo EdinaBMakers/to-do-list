cbd8c50eb65b5093325f34ac1ebda3cf26eb92ef
Edina Bolkeny <bolkeny.edina@gmail.com>
2019-11-16 13:07:17 +0000

    Setup route to create task

--- api/routes/toDoListRoutes.js
+++ api/routes/toDoListRoutes.js
@@ -4,5 +4,6 @@ module.exports = function(app) {
   const toDoList = require('../controllers/toDoListControllers');
 
   app.route('/tasks')
-    .get(toDoList.listAllTasks);
+    .get(toDoList.listAllTasks)
+    .post(toDoList.createAtask);
 };
