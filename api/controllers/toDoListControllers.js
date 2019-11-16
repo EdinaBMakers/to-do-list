221a4741b951b060d6809b7eb08b37b02257d46c
Edina Bolkeny <bolkeny.edina@gmail.com>
2019-11-16 12:51:50 +0000

    Retrieve all tasks

--- api/controllers/toDoListControllers.js
+++ api/controllers/toDoListControllers.js
@@ -1,1 +1,11 @@
-// TODO: listAllTasks
+'use strict';
+
+const mongoose = require('mongoose');
+const Task = mongoose.model('Tasks');
+
+exports.listAllTasks = function(req, res) {
+  Task.find({}, function(err, task) {
+    if (err) { return res.send(err); }
+    res.json(task);
+  }); 
+};
--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 const express = require('express');
 const app = express();
 const routes = require('./api/routes/toDoListRoutes');
+const mongoose = require('mongoose');
+const Task = require('./api/models/toDoListModel');
+const bodyParser = require('body-parser');
+
+mongoose.set('useUnifiedTopology', true);
+mongoose.set('useNewUrlParser', true);
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/ToDodb');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 routes(app);
 
